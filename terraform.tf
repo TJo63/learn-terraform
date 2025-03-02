2fd359f650fc27f36cd9e56ad8af2aa72470aa87
Tryggve Johannesson <tryggve.johannesson@consultant.volvo.com>
2025-03-02 19:59:02 +0100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,14 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+/*  cloud {
+    organization = "tjohoo"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
